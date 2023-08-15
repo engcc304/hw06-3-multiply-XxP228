92ed6b9cf2d03da167fccdfff165840f66db1635
XxP228 <kamochnok.2548@gmail.com>
2023-08-16 01:53:04 +0700

    hw6-3

--- Homework/HW06/hw06-3-Multiply.cpp
+++ Homework/HW06/hw06-3-Multiply.cpp
@@ -75,3 +75,23 @@
         6 x 9 = 54
 
 */
+#include <stdio.h>
+
+int main() {
+    int start, end;
+
+    printf("Enter start: ");
+    scanf("%d", &start);
+
+    printf("Enter end: ");
+    scanf("%d", &end);
+
+    for (int i = start; i <= end; i++) {
+        printf("\n");
+        for (int j = 1; j <= 9; j++) {
+            printf("%d x %d = %d\n", i, j, i * j);
+        }
+    }
+
+    return 0;
+}
